9d0999b531f2fa2a98458b6d4fc031787adf6e6a
Kalpesh Patel <kalpeshp@vmware.com>
2020-09-05 22:48:55 +0530

    Initial Commit

---
+++ lib/models/category.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  String id;
+  String title;
+  Color color;
+
+  Category({
+    @required this.id,
+    @required this.title,
+    this.color = Colors.orange,
+  });
+}
